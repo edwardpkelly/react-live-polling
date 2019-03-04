bd762e3d3590d7ebf5553e6087d35778181bb07b
Edward P. Kelly <edwardp.kelly@yahoo.com>
2019-03-04 12:00:13 -0500

    Setting up socket.io
    Adding socket.io to the app-server and the client.

--- app-server.js
+++ app-server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const PORT = 3000;
 const app = express();
+const server = require('http').Server(app);
+const socketio = require('socket.io')(server);
+const PORT = 3000;
 
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Live Polling App Running on Port ${PORT}`);
+});
 
-})
+socketio.on('connection', (socket) => {
+    console.log('Connected %s!', socket.id);
+});
--- src/js/components/container/ReactContainer.jsx
+++ src/js/components/container/ReactContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import socketio from 'socket.io-client';
 
 class ReactContainer extends Component {
     constructor(props) {
@@ -6,6 +7,16 @@ class ReactContainer extends Component {
         this.state = { };
 
         this.handleChange = this.handleChange.bind(this);
+        this.connect = this.connect.bind(this);
+    }
+
+    componentWillMount() {
+        this.socket = socketio.connect('http://localhost:3000');
+        this.socket.on('connect', this.connect);
+    }
+
+    connect() {
+        alert(`Connected: ${this.socket.id}`);
     }
 
     handleChange(event) {
